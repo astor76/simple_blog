71ff2b8cd402953a8d5fd4fd4abb133bcbfc9dac
Иван Мигунов <qussaro@gmail.com>
2017-03-21 09:47:59 +0300

    View example

--- testblog/views.py
+++ testblog/views.py
@@ -5,6 +5,20 @@ from testblog.models import Post
 from django.views.generic import ListView
 from django.views.generic import DetailView
 
+#from .models import Post
+#
+#def post_list(request):
+#    """
+#    Отображение списка постов
+#    """
+#    ctx = {
+#        'title': 'Список постов',
+#        'posts': Post.objects.all(),
+#    }
+#    return render(request, '<путь к шаблону>',  ctx)
+# Останется задать данному представлению url правило и готово!
+# https://docs.djangoproject.com/en/1.10/intro/tutorial03/#a-shortcut-render
+
 class PostsListView(ListView):
     model = Post
 
