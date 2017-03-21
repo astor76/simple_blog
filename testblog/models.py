f5ceda4091a48294ecfc68aa8e32f4f1df0e33dc
astor <astor0_0@mail.ru>
2017-03-22 01:03:32 +0300

    добавленны комментарии

--- testblog/models.py
+++ testblog/models.py
@@ -3,19 +3,19 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=255)  # заголовок поста
-    datetime = models.DateTimeField(u'Дата публикации')  # дата публикации
+    datetime = models.DateTimeField(verbose_name='Дата публикации')  # дата публикации
     content = models.TextField(max_length=10000)  # текст поста
 
-    def __unicode__(self):
+    def __str__(self):  # возвращает строковое представление объекта
         return self.title
 
-    def get_absolute_url(self):
+    def get_absolute_url(self):  # возвращает строку, которую можно использовать в HTTP запросе
         return "/blog/%i" % self.id
 
 
 class Comment(models.Model):
     name = models.CharField(max_length=255)  # имя комментатора указывается при создании поста
-    datetime = models.DateTimeField(u'Дата комментирования')  # дата комментирования
+    datetime = models.DateTimeField(verbose_name='Дата комментирования')  # дата комментирования
     content = models.TextField(max_length=1000)  # текст комментария
 
 
